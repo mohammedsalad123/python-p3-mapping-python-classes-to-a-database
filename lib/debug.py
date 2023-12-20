c2f0d1437f5bc08ab737004afaab1af68cb7dda6
mohammed <mohammed.salad@student.moringaschool.com>
2023-12-20 13:20:09 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -6,3 +6,14 @@ from song import Song
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+from lib import CONN, CURSOR
+from lib.song import Song
+
+def reset_db():
+    Song.drop_table()
+    Song.create_table()
+    
+
+import pytest; pytest.set_trace()
+
